18d4dddae1c9e2007e8b61b47f2792380ed841d9
MaherAlhijile <maher.alhijile@gmail.com>
2024-07-24 03:09:17 +0300

    justCap

--- fullScreenPixelalter.c
+++ fullScreenPixelalter.c
@@ -28,87 +28,15 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         bits = CreateCompatibleBitmap(screen, 1920, 1080);
         SelectObject(capture, bits);
         SetWindowDisplayAffinity(window, WDA_EXCLUDEFROMCAPTURE);
-        // Capture the screen into the bitmap
         BitBlt(capture, 0, 0, 1920, 1080, screen, 0, 0, SRCCOPY);
-        // Apply pixelation effect by scaling down and then up
-        // Adjust pixel size as needed
-        switch (mode)
-        {
-        case (1):
-        {
-            SetStretchBltMode(capture, BLACKONWHITE);
-            SetStretchBltMode(GetDC(window), BLACKONWHITE);
-            break;
-        }
-        case (2):
-        {
-            SetStretchBltMode(capture, COLORONCOLOR);
-            SetStretchBltMode(GetDC(window), COLORONCOLOR);
-            break;
-        }
-        case (3):
-        {
-            SetStretchBltMode(capture, HALFTONE);
-            SetStretchBltMode(GetDC(window), HALFTONE);
-            break;
-        }
-
-        case (4):
-        {
-            SetStretchBltMode(capture, WHITEONBLACK);
-            SetStretchBltMode(GetDC(window), WHITEONBLACK);
-            break;
-        }
-
-        default:
-            break;
-        }
         StretchBlt(capture, 0, 0, 1920 / pixelSize, 1080 / pixelSize, capture, 0, 0, 1920, 1080, SRCCOPY);
         StretchBlt(GetDC(window), 0, 0, 1920, 1080, capture, 0, 0, 1920 / pixelSize, 1080 / pixelSize, SRCCOPY);
         DeleteObject(bits);
         DeleteDC(capture);
         ReleaseDC(NULL, screen);
-
         return 0;
     }
 
-    case WM_KEYDOWN:
-    {
-        switch (wParam)
-        {
-        case VK_UP:
-            pixelSize++;
-            break;
-
-        case VK_DOWN:
-            if (pixelSize > 1)
-            {
-                pixelSize--;
-            }
-            break;
-
-        case 0x31:
-            mode = 1;
-            break;
-
-        case 0x32:
-            mode = 2;
-            break;
-
-        case 0x33:
-            mode = 3;
-            break;
-
-        case 0x34:
-            mode = 4;
-            break;
-
-        default:
-            break;
-        }
-
-        return 0;
-    }
     case WM_DESTROY:
         PostQuitMessage(0);
         return 0;
